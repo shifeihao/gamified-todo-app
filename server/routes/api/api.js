86572214481df77c82c52af4feb7def67afe06ed
Feihao Shi <shifeihao@outlook.com>
2025-05-12 15:32:08 +1200

    equipment

--- server/routes/api/api.js
+++ server/routes/api/api.js
@@ -5,12 +5,11 @@ import taskRoutes from "./task.js";
 import levelRoutes from "./level.js";
 import shopRoutes from "./shop.js";
 import achievementRoutes from "./achievement.js";
-import dungeonRoutes from "./dungeon.js"
-import characterRoutes from './character.js';
-import templateRoutes from './template.js';
+import dungeonRoutes from "./dungeon.js";
+import characterRoutes from "./character.js";
+import inventoryRoutes from "./inventory.js";
 
 const router = express.Router();
-
 router.use("/users", userRoutes);
 router.use("/cards", cardRoutes);
 router.use("/tasks", taskRoutes);
@@ -18,7 +17,7 @@ router.use("/levels", levelRoutes);
 router.use("/shop", shopRoutes);
 router.use("/achievements", achievementRoutes);
 router.use("/dungeon", dungeonRoutes); // 确保 dungeonRoutes 已经定义并导入
-router.use('/character', characterRoutes);
-router.use('/templates', templateRoutes);
+router.use("/character", characterRoutes);
+router.use("/inventory", inventoryRoutes);
 
 export default router;
